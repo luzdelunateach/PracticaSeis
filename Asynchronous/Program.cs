f1f6c573bc7c4a40d293ffabeadd2f8cbf0e8c9f
Luz de Luna GG <miscluzdeluna@gmail.com>
2022-04-06 14:47:27 -0500

    Actualizacion de ejemplos de Task

---
+++ Asynchronous/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Asynchronous
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+           
+            Console.WriteLine("Loading...");
+            Asynchronous.WaitForIt();
+            await Asynchronous.WaitForIt2(); //await es una promesa pero eventualmente voy a regresar ati con una promesa
+            //async esperando al servidor mientras 
+            var timeElapsed = await Parallelism.Main(); //await desemcapsula es tarea
+            Console.WriteLine($"Ambos rpocesos finalizan despues {timeElapsed / 100m} seconds");
+        }
+        
+    }
+    public class Asynchronous
+    {
+        public static async Task WaitForIt()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(5)); //await
+            Console.WriteLine("Five seconds complete");
+        }
+
+        /*public static string Something()
+        {
+
+        }*/
+        public static async Task WaitForIt2()
+        {
+            await Task.Delay(10000);
+            Console.WriteLine("Ten secoinds complete");
+        }
+    }
+    //Parelilismo
+    public class Parallelism
+    {
+        public static async Task<long> Main()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var task1 = Process1(); //await Process1(); aqui si importa el
+            var task2 = Process2(); //await Process2();
+           
+            //var task1 =
+            await Task.WhenAll(task1, task2); //respuestas de las ejecuciones en las lineas 33 y 34 - los dos se ejecutan al mismo tiempo y el hilo principal se detetiene
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        public static async Task Process1() //task abre los hilos
+        {
+            await Task.Run(() =>
+            {
+                Thread.Sleep(4000);
+            });
+        }
+
+        public static async Task Process2()
+        {
+            await Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+            });
+        }
+    }
+}
---
+++ WorkingWithDates/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WorkingWithDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now.Date;
+            var x = date.ToString("dd/mm/yyyy"); //El formato que yo dese manejar en mi base de datos, para hacer comparaciones, aveces es necesario tener un mismo formato
+            //para evitar problemas con la comparacion
+
+            Console.WriteLine(x);
+        }
+    }
+}
