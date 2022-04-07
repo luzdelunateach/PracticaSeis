927ddca612851b9de9e38aacf7a53c238b5105d2
Luz de Luna GG <miscluzdeluna@gmail.com>
2022-04-07 09:17:20 -0500

    Proyecto pendiente terminado

--- Reminder/Program.cs
+++ Reminder/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Globalization;
 using System.Threading.Tasks;
 
 namespace Reminder
@@ -8,47 +9,93 @@ namespace Reminder
     {
         static void Main(string[] args)
         {
-            menu();
-
+            startMenu();
         }
-        static void menu()
+        static async Task startMenu()
         {
-   
-           String dato = "";
-           Console.WriteLine("What's your event name?");
-           dato = Console.ReadLine();
-           if (String.IsNullOrEmpty(dato))
+            CultureInfo enUS = new CultureInfo("en-US");
+            DateTime dateValue;
+            DateTime dateValue2;
+            Console.WriteLine("What's your event name?");
+            var evento = Console.ReadLine();
+            if (String.IsNullOrEmpty(evento))
                Console.WriteLine("Error");
-           else{
+            else{
                Console.WriteLine("When will it start? (Please indicate the date with the next format: mm/dd/yyyy)");
-               var date = Console.ReadLine();
-               Console.WriteLine("Please indicate how many days will Star Reminder You");
-               string d = Console.ReadLine();
-               if(Int32.TryParse(d, out int days))
+               var date1 = Console.ReadLine();
+               if (DateTime.TryParseExact(date1, "MM/dd/yyyy", enUS, DateTimeStyles.None, out dateValue))
+               {
+                    Console.WriteLine("Please indicate how many days will Star Reminder You");
+                    string d = Console.ReadLine();
+                    if (Int32.TryParse(d, out int days))
+                    {
+                        Console.WriteLine("Please indicate the current date.");
+                        var date2 = Console.ReadLine();
+                        if (DateTime.TryParseExact(date2, "MM/dd/yyyy", enUS, DateTimeStyles.None, out dateValue2))
+                        {
+                            if (dateValue2 >= dateValue)
+                            {
+                                Console.WriteLine("The current date can't be higher than the event date.");
+                            }
+                            else
+                            {
+                                Console.Clear();
+
+                                DateTime currentDate = await Asynchronous.Reminder(dateValue, dateValue2, days);
+                                await Asynchronous.EventDay(currentDate,dateValue);
+
+                                Console.Clear();
+                                Console.WriteLine($"Hey, {evento} is today!!!");
+
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please insert a valid date format.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please insert an integer");
+                    }
+                }else
                 {
-                    compareDates(date, days);
+                    Console.WriteLine("Please insert a valid date format.");
+                    return;
                 }
-               
            }
            
-        }
-
-        public static void compareDates(String date, int days)
-        {
-            DateTime dateI = DateTime.ParseExact(date, "dd/MM/yyyy", null);
-            DateTime dateF = dateI.AddDays(days);
-
-            Console.WriteLine(dateF);
         }
             
     }
     public class Asynchronous
     {
-        public static async Task WaitForIt()
-        { 
-            await Task.Delay(TimeSpan.FromSeconds(100000)); //await
-            Console.WriteLine("Five seconds complete");
-           
+       
+        public static async Task<DateTime> Reminder(DateTime date, DateTime date2, int days)
+        {
+           date = date.AddDays(-days);
+           while (date2 < date)
+            {
+                Console.WriteLine(date2.ToString("MM/dd/yyyy"));
+                //await Task.Delay(1000);
+                Task.Delay(3000).Wait();
+                date2 = date2.AddDays(1);
+                Console.Clear();
+            }
+           return date2;
+        }
+
+        public static async Task<DateTime> EventDay(DateTime currentDate1, DateTime dateEvent)
+        {
+            while (currentDate1 < dateEvent )
+            {
+                Console.WriteLine($"Only {dateEvent.Day-currentDate1.Day} days to the event!");
+                //await Task.Delay(TimeSpan.FromSeconds(3)); //Task.Delay(1000);TimeSpan.FromSeconds(1)
+                Task.Delay(3000).Wait();
+                currentDate1 = currentDate1.AddDays(1);
+                Console.Clear();
+            }
+            return dateEvent;
         }
     }
 }
