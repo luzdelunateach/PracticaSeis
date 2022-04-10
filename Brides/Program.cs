65588e4370eb0c2c095f64fba8c45dc4b5c1c8f0
Luz de Luna GG <miscluzdeluna@gmail.com>
2022-04-09 21:19:55 -0500

    Ejercicio pendiente por subir

---
+++ Brides/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Brides
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Brides();
+        }
+        
+        public static void Brides()
+        {
+            int[] row = new int[]{4,1,2,3,5};
+            int brides = 0;
+            int r = 0;
+            for (int i=0;i<row.Length;i++)
+            {
+                r = row[i] - (i + 1);
+                if (r > 2)
+                {
+                    Console.WriteLine("Too Chaotic xD");
+                    break;
+                }
+                else if(r > 0 && r < 2)
+                {
+                    brides++;
+                }
+            }
+            Console.WriteLine($"Brides: {brides}");
+
+        }
+    }
+}
