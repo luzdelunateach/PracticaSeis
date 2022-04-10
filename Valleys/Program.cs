3f126e7762709f1337b0c2439289cce93c2e11a2
Luz de Luna GG <miscluzdeluna@gmail.com>
2022-04-09 23:09:09 -0500

    Proyecto Faltante

---
+++ Valleys/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Valleys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateValleys() ;
+        }
+
+        public static void CalculateValleys()
+        {
+            string[] s = new string[]{ "U","D","D","U","D","U","U"};
+            int level = 0;
+            int valley = 0;
+            for(int i=0; i<s.Length; i++)
+            {
+                if (s[i].Equals("U"))
+                {
+                    level++;
+                    
+                }
+                else if (s[i].Equals("D"))
+                {
+                    level--;
+                    if(level == 0)
+                    {
+                        valley++;
+                    }
+                }
+                
+            }
+            Console.WriteLine($"Numero de villas es:{valley}");
+        }
+    }
+}
