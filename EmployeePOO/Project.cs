20d798fc65cdd1d3191862db058769385860ba91
Luz de Luna GG <miscluzdeluna@gmail.com>
2022-04-10 19:24:46 -0500

    cambios

---
+++ EmployeePOO/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePOO
+{
+    class Project
+    {
+        public int IdProject { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+
+       
+    }
+}
