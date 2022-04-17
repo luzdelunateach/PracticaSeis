a36a2b362540bf52f8698f26ac2ca8c0910cb7b9
Luz de Luna GG <miscluzdeluna@gmail.com>
2022-04-16 23:38:36 -0500

    Actualizacion de productos

--- eShop/eShop/eShopConsoleReports.cs
+++ eShop/eShop/eShopConsoleReports.cs
@@ -10,25 +10,25 @@ namespace eShop
         {
             Console.Clear();
             Console.WriteLine("Elige una opcion");
-            Console.WriteLine("1. Top 5 de productos mas caros ordenados por precio");
-            Console.WriteLine("2. Productos con 5 unidades o menos ordenados por unidades");
-            Console.WriteLine("3. Nombre de productos por marcas ordenados por nombre");
-            Console.WriteLine("4. Agrupación de departamentos con subdepartamentos y nombres de productos");
+            Console.WriteLine("1. Reporte de Top 5 de productos mas caros ordenados por precio");
+            Console.WriteLine("2. Reporte de Productos con 5 unidades o menos ");
+            Console.WriteLine("3. Reporte de productos por marcas ordenados por nombre");
+            Console.WriteLine("4. Reporte de departamentos con subdepartamentos y nombres de productos");
             Console.WriteLine("5. Regresar");
 
             switch (Console.ReadLine())
             {
                 case "1":
-                    FiveExpensiveProductsOrderedByPrice();
+                    FiveExpensiveProducts();
                     break;
                 case "2":
                     FiveProductsorLess();
                     break;
                 case "3":
-                    ProductsByBrandOrderedByProductName();
+                    ProductsByBrand();
                     break;
                 case "4":
-                    GroupDepartmentGroupSubdepartmentProducts();
+                    SubdepartmentProducts();
                     break;
                 case "5":
                 default:
@@ -39,13 +39,13 @@ namespace eShop
         #endregion
 
         #region Reportes
-        private void FiveExpensiveProductsOrderedByPrice()
+        private void FiveExpensiveProducts()
         {
             var listProducts = _reportService.Top5ExpensiveProductsOrderedByPrice();
 
             listProducts.ForEach(lp =>
             {
-                Console.WriteLine($"Producto: {lp.Name},  Precio: {lp.Price}");
+                Console.WriteLine($"Producto: {lp.Name}  \t  Precio: {lp.Price}");
             });
         }
 
@@ -55,11 +55,11 @@ namespace eShop
 
             listProducts.ForEach(lp =>
             {
-                Console.WriteLine($"Producto: {lp.Name},  Precio: {lp.Price}");
+                Console.WriteLine($"Producto: {lp.Name}  \t  Precio: {lp.Price}");
             });
         }
 
-        private void ProductsByBrandOrderedByProductName()
+        private void ProductsByBrand()
         {
             var listProducts = _reportService.ProductsByBrandOrderedByProductName();
             listProducts.ForEach(b =>
@@ -72,10 +72,10 @@ namespace eShop
             });
         }
 
-        private void GroupDepartmentGroupSubdepartmentProducts()
+        private void SubdepartmentProducts()
         {
-            var departments = _reportService.GroupDepartmentGroupSubdepartmentProducts();
-            foreach (var department in departments)
+            var d = _reportService.GroupDepartmentGroupSubdepartmentProducts();
+            foreach (var department in d)
             {
                 Console.WriteLine($"{department.Department}");
                 foreach (var subdepartment in department.Subdepartaments)
